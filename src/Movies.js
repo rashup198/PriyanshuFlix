38a0038355c44d16361783a8def4e677c310f0ac
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-21 23:30:07 +0530

    updated

--- src/Movies.js
+++ src/Movies.js
@@ -6,37 +6,45 @@ import { NavLink } from 'react-router-dom';
 const Movies = () => {
   const {movie} = useContext(AppContext);
   return (
-    <div>
-      {
-        movie.map((curmovie)=>{
-          return(
-           <section className='movie-page'>
-            <div className='grid grid-4-col container'>
-            {
-              movie.map((curmovie)=>{
-                const {imdbID,Poster,Title} = curmovie;
-                return(
-                  <NavLink to={`movie/${imdbID}`} key={imdbID}>
-                      <div className='card'>
-                      <div className='card-info'>
-                          <h2>
-                            {Title.substring(0,20).length>15 ?`${Title}....` : Title }
-                          </h2>
-                          <img src={Poster} alt='img'></img>
-                      </div>
-                      </div>
-                  </NavLink>
-                )
-              })
-            }
+    const { movie, isLoading } = useGlobalContext();
+  if (isLoading) {
+    return <div className="loading">Loading....</div>;
+  }
 
-            </div>
-           </section>
-          )
-        })
-      }
-    </div>
-  )
+  /* plz subsribe to thapa technical channel 
+          https://www.youtube.com/thapatechnical
+         */
+
+  return (
+    <>
+      {/* if movie is present then only show data else remain as it is  */}
+      <section className="movie-page">
+        <div className="grid grid-4-col">
+          {movie
+            ? movie.map((curMovieElem) => {
+                const { imdbID, Title, Poster } = curMovieElem;
+                const movieName = Title.substring(0, 15);
+
+                return (
+                  <NavLink to={`movie/${imdbID}`} key={imdbID}>
+                    <div className="card">
+                      <div className="card-info">
+                        <h2>
+                          {movieName.length > 13
+                            ? `${movieName}...`
+                            : movieName}
+                        </h2>
+                        <img src={Poster === "N/A" ? imgUrl : Poster} alt="#" />
+                      </div>
+                    </div>
+                  </NavLink>
+                );
+              })
+            : ""}
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Movies
--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from './Context';
+  
+
 
 const Search = () => {
+
+    function changeHandler(event) {
+      setQuery(event.target.value)
+    }
+  const {query, setQuery,isError} = useContext(AppContext)
   return (
     <div>
-      
+      <section className='search-section'>
+      <h2>Search Your Movie</h2>
+      <form action='#' onSubmit={(e)=>e.preventDefault()}>
+      <div>
+        <input type='text' placeholder='Search here' value={query}
+        onChange={changeHandler}></input>
+      </div>
+
+      </form>
+      <div className='card-error'>
+          <p>{isError.show && isError.msg}</p>
+      </div>
+
+      </section>
     </div>
   )
 }
