9d4291ed8963815998e625bdec1cd09817f35b07
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-21 23:26:59 +0530

    updated Context.js

--- src/Context.js
+++ src/Context.js
@@ -1,16 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 
 // Context(wareHouse)
 // provider(delivery boy)
 // consumer useContext(you)
 
-const API_URL =`http://www.omdbapi.com/?i=tt3896198&apikey=1358f537&s=titanic`
+const API_URL =`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_API_KEY}`
 
 const AppContext = React.createContext();
 
 
 const AppProvider = ({children}) => {
+
+  const [isloading, setIsLoading] = useState(true);
+  const [movie, setMovie]= useState([]);
+  const [query, setQuery]= useState("titanic")
+  const [isError, setIsError] = useState({show:"false", msg:""})
 
   const getMovies = async (url) => {
 
@@ -18,6 +24,17 @@ const AppProvider = ({children}) => {
         const res = await fetch(url);
         const data= await res.json();
         console.log(data);
+
+        if(data.Response==="True"){
+          setMovie(data.Search)
+          setIsLoading(false);
+        }
+        else{
+          setIsError({
+            show:true,
+            msg:data.error,
+          })
+        }
       } 
       catch (error) {
         console.log("Error Found" + error);  
@@ -28,12 +45,12 @@ const AppProvider = ({children}) => {
 
 
   useEffect(() => {
-    getMovies(API_URL);
-  },[])
+    getMovies(`${API_URL}&s=${query}`);
+  },[query])
 
 
   return (
-    <AppContext.Provider value={"Rashu"}>
+    <AppContext.Provider value={{isloading,movie,query, setQuery, isError}}>
       {children}
     </AppContext.Provider>
   )
