39321e109f6cb4235d52eb7b0805b6511d7ea307
Priyanshu Pandey <88549100+rashup198@users.noreply.github.com>
2023-06-21 21:51:20 +0530

    updated  App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './Home';
+import { Routes, Route } from "react-router-dom";
+import SingleMovie from './SingleMovie';
+import Error from './Error';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="movie/:id" element={<SingleMovie></SingleMovie>}></Route>
+        <Route path="*" element={<Error></Error>}></Route>
+
+      </Routes>
+    
+      
     </div>
   );
 }
